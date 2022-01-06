d49dcec491ef792159929a70de39924c00c5c5c4
HyunjuIm <dear.jjwim@gmail.com>
2022-01-06 15:38:23 +0900

    MotionLayout 화면 전환 UI 구성

--- app/src/main/java/com/fighting/youtube/MainActivity.kt
+++ app/src/main/java/com/fighting/youtube/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, PlayerFragment())
+            .commit()
     }
 }
